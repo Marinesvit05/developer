9c0fe46eaacc813eed461701ad9ff1f73183433a
Maria Nesvit <maria.nesvit@example.com>
2025-09-26 21:00:34 +0200

    Add GreetUser function to Program.cs

--- Program.cs
+++ Program.cs
@@ -7,4 +7,8 @@ class Program
         Console.WriteLine("Hello, World!");
         GreetUser("User");
     }
+    static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
 }
